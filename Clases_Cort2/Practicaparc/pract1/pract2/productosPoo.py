0cdef2063f48e54619b536b0dbf41211b545d4cc
JUANDA784 <125518696+JUANDA784@users.noreply.github.com>
2023-04-25 11:43:46 -0500

    Subo practica del parcial

--- Clases_Cort2/Practicaparc/pract1/pract2/productosPoo.py
+++ Clases_Cort2/Practicaparc/pract1/pract2/productosPoo.py
@@ -1,10 +1,12 @@
-#2:32
 class Archivo():
     def __init__(self):
 
         self.__leer = None
         self.__producto = None
         self.__precio = None
+        self.__productos = []
+        self.__producto_list = []
+        self.__valor_neto = 0
 
 #--------------------------- getters -----------------------------
 
@@ -28,63 +30,46 @@ class Archivo():
         for linea in data:
             valores.append(linea.strip('\n').split(','))
 
-    def setProducto(self, valores):
+        self.__productos = valores
+
+    def setProducto(self):
         self.__producto = input('Ingrese el producto que desea calcular o dale (exit): ')
 
         if self.__producto != 'exit':
-
-            list_product = []
-
-            for i in valores:
-                if len(i) < 2:
-                    continue
-                if self.__producto == i[1]:
-                    list_product.append(i)
-
-        print(list_product)
+            for producto in self.__productos:
+                if self.__producto == producto[1]:
+                    self.__producto_list = producto
+                    return
 
     def setPrecio(self):
         self.__precio = int(input('Ingrese el precio del producto: '))
 
-#--------------------------- metodos ---------------------------------------
-    def leerArchivo(self):
-        list_product = []
-       
-        return list_product
-    
-    def __valorNeto(self, list_product):
-        valor_neto = 0
-        if len(list_product) >= 3:
-            valor_neto = round(self.__precio / (1 + float(list_product[2])), 2)
-        else:
-            print("La lista de productos no tiene suficientes elementos")
-        return valor_neto
+    #--------------------------- metodos ---------------------------------------
+    def __valorNeto(self):
+        self.__valor_neto = round(self.__precio / (1 + float(self.__producto_list[2])), 2)
+        return self.__valor_neto
         
-    def getNeto(self, list_product):
-        return self.__valorNeto(list_product)
+    def getNeto(self):
+        return self.__valorNeto()
 
 
-    def __ivaProduct(self, valor_neto, list_product):
-        if len(list_product) >= 3:
-            iva = round((valor_neto * float(list_product[2])),2)
-            return iva
-        else:
-            print("La lista de productos no tiene suficientes elementos")
-    
-    def getivaDelProducto(self, valor_neto, list_product):
-        return self.__ivaProduct(valor_neto, list_product)
+    def __ivaProduct(self):
+        iva = round((self.__valor_neto * float(self.__producto_list[2])),2)
+        return iva
+
+    def getivaDelProducto(self):
+        return self.__ivaProduct()
 
 def main():
     iva = Archivo()
     iva.setLeer()
-    valores = iva.getLeer()
-    iva.setProducto(valores)
+    iva.getLeer()
+    iva.setProducto()
     iva.setPrecio()
-    list_product = iva.leerArchivo()
-    valor_neto = iva.getNeto(list_product)
+    iva.getNeto()
 
-    print(f'IVA = {valor_neto}\n'+ \
-          f'El valor neto o precio sin IVA del producto es = {iva.getivaDelProducto(valor_neto, list_product)}')
+    print(f'IVA = {iva.getivaDelProducto()}\n'+ \
+          f'El valor neto o precio sin IVA del producto es = {iva.getNeto()}')
 
 
 if __name__=='__main__': 
